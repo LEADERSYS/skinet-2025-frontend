864efba8a539502716e6522cd699b5476243e575
Iván Ramírez <leadersysmx@gmail.com>
2026-04-14 17:00:48 -0600

    End of Section 16

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5249/api/'
+    apiUrl: 'https://localhost:5249/api/',
+    stripePublicKey: 'pk_test_51TEZvZAdOWgSssMfcGjFbwnTN2GBTgqJ9ta6MkLX1BVePOGo2cahK0nI5FLv5d7a7QPjl0sXgyIklN0VGSQErUch004oNKdAIj'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51TEZvZAdOWgSssMfcGjFbwnTN2GBTgqJ9ta6MkLX1BVePOGo2cahK0nI5FLv5d7a7QPjl0sXgyIklN0VGSQErUch004oNKdAIj'
 };
